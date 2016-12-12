0142a2a06629db8a78709ed47f694ca365ef993d
Kristin Ottofy <kottofy@outlook.com>
2016-12-12 15:54:52 -0500

    updated sensors

--- SensorApp/MainPage.xaml.cs
+++ SensorApp/MainPage.xaml.cs
@@ -28,12 +28,13 @@ namespace SensorApp
         private const int TEMP_SENSOR_PIN = 19;
         private const int FLAME_SENSOR_PIN = 13;
         private const int METAL_SENSOR_PIN = 06;
-
-        private GpioPin lightPin, tempPin, flamePin, metalPin;
-
-        //private DispatcherTimer lightPinTimer, tempPinTimer, flamePinTimer, metalPinTimer;
+        private const int GAS_SENSOR_PIN = 05;
+        private const int KNOCK_SENSOR_PIN = 21;
+        private GpioPin lightPin, tempPin, flamePin, metalPin, gasPin, knockPin;
         private DispatcherTimer sensorTimer;
-        private const int SENSOR_CHECK_TIME = 2000;    // number of milliseconds to get input from the tilt sensor
+        private const int SENSOR_CHECK_TIME = 1000;    // number of milliseconds to get input from the tilt sensor
+        private string deviceId = "Device01";
+        private int measurement = 0;
 
         public MainPage()
         {
@@ -57,27 +58,46 @@ namespace SensorApp
 
         private async void Sensor_Timer_Tick(object sender, object e)
         {
-            string lightPinRead, tempPinRead, flamePinRead, metalPinRead;
+            string lightPinRead, tempPinRead, flamePinRead, metalPinRead, gasPinRead, knockPinRead;
 
-            lightPinRead = (lightPin.Read()).ToString();
+            getLight();
+
+            //lightPinRead = getLight().ToString();
+            //lightPinRead = measurement.ToString();
             tempPinRead = (tempPin.Read()).ToString();
             flamePinRead = (flamePin.Read()).ToString();
             metalPinRead = (metalPin.Read()).ToString();
+            gasPinRead = (gasPin.Read()).ToString();
+            knockPinRead = (knockPin.Read()).ToString();
 
-            Debug.WriteLine("Light:" + lightPinRead);
+            //Debug.WriteLine("Light:" + lightPinRead);
             Debug.WriteLine("Temp:" + tempPinRead);
-            Debug.WriteLine("Flame:" + flamePinRead);
-            Debug.WriteLine("Metal:" + metalPinRead);
+            Debug.WriteLine("Gas:" + gasPinRead);
+            Debug.WriteLine("Knock:" + knockPinRead);
+            
+            if (flamePinRead.Equals("High"))
+            {
+                Debug.WriteLine("Flame:" + flamePinRead);
+                AzureIoTHub.SendDeviceToCloudMessageAsync(JSONify("flameSensor", flamePinRead, DateTime.Now));
+            }
 
-            AzureIoTHub.SendDeviceToCloudMessageAsync(JSONify("DeviceID", "lightSensor", lightPinRead, DateTime.Now));
+            if (metalPinRead.Equals("High"))
+            {
+                Debug.WriteLine("Metal:" + metalPinRead);
+                AzureIoTHub.SendDeviceToCloudMessageAsync(JSONify("metalSensor", metalPinRead, DateTime.Now));
+            }
 
+            AzureIoTHub.SendDeviceToCloudMessageAsync(JSONify("tempSensor", tempPinRead, DateTime.Now));
+            AzureIoTHub.SendDeviceToCloudMessageAsync(JSONify("gasSensor", gasPinRead, DateTime.Now));
+            AzureIoTHub.SendDeviceToCloudMessageAsync(JSONify("knockSensor", knockPinRead, DateTime.Now));
         }
-        private string JSONify(string id, string sensorName, string sensorValue, DateTime outputTime)
+
+        private string JSONify(string sensorType, string sensorValue, DateTime outputTime)
         {
             //do some fancy JSON stuff to convert the message to a JSON
             Dictionary<string, string> json_message = new Dictionary<string, string>();
-            json_message.Add("Id", id) ;
-            json_message.Add("SensorName", sensorName);
+            json_message.Add("DeviceId", deviceId) ;
+            json_message.Add("SensorType", sensorType);
             json_message.Add("SensorValue", sensorValue);
             json_message.Add("OutputTime", outputTime.ToString());
             return JsonConvert.SerializeObject(json_message);
@@ -92,12 +112,34 @@ namespace SensorApp
             tempPin = gpio.OpenPin(TEMP_SENSOR_PIN);
             flamePin = gpio.OpenPin(FLAME_SENSOR_PIN);
             metalPin = gpio.OpenPin(METAL_SENSOR_PIN);
+            gasPin = gpio.OpenPin(GAS_SENSOR_PIN);
+            knockPin = gpio.OpenPin(KNOCK_SENSOR_PIN);
 
-            lightPin.SetDriveMode(GpioPinDriveMode.Input);
+            getLight();
+            
             tempPin.SetDriveMode(GpioPinDriveMode.Input);
             flamePin.SetDriveMode(GpioPinDriveMode.Input);
             metalPin.SetDriveMode(GpioPinDriveMode.Input);
+            gasPin.SetDriveMode(GpioPinDriveMode.Input);
+            knockPin.SetDriveMode(GpioPinDriveMode.Input);
             
+        }
+
+        private async void getLight()
+        {
+            lightPin.SetDriveMode(GpioPinDriveMode.Output);
+            lightPin.Write(GpioPinValue.Low);
+            await Task.Delay(500);
+            lightPin.SetDriveMode(GpioPinDriveMode.Input);
+
+            measurement = 0;
+
+            while (lightPin.Read() == GpioPinValue.Low)
+                measurement += 1;
+
+            Debug.WriteLine("Light:" + measurement);
+            
+            await AzureIoTHub.SendDeviceToCloudMessageAsync(JSONify("lightSensor", measurement.ToString(), DateTime.Now));
         }
     }
 }
